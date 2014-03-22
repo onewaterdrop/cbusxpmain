717d5c2cc2c5ecf11c12cd8497f08f798ec17c73
Tim Kou <osu.flyer2010@gmail.com>
2014-03-22 17:18:21 -0400

    my setup redirect

---
+++ bbs/conf/founder.php
@@ -0,0 +1,5 @@
+<?php
+  return array(
+	'admin' => 'c783804cb45f1998333d475471f7642a',
+);
+?>
---
+++ bbs/conf/windidconfig.php
@@ -0,0 +1,10 @@
+<?php
+  return array(
+	'windid' => 'local',
+	'serverUrl' => 'http://columbusxp.com/bbs',
+	'clientId' => '1',
+	'clientKey' => '59ccbe71b75884f1ffdbb02ea8e5e730',
+	'clientDb' => 'mysql',
+	'clientCharser' => 'utf8',
+);
+?>
